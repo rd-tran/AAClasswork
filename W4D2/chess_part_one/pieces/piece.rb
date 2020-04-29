2ba1810b3b369704cf2b24c4d465575781756e92
Ryan Tran <ryandtran@hotmail.com>
2020-04-29 09:20:50 -0700

    Added files for each piece

--- W4D2/chess_part_one/pieces/piece.rb
+++ W4D2/chess_part_one/pieces/piece.rb
@@ -1,15 +1,41 @@
 class Piece
+  attr_reader :board, :color
+  attr_accessor :pos
+
   def initialize(pos, board, color)
     @pos = pos
     @board
     @color
   end
 
-  def pos
-    @pos
+  # * Define to_s method
+  # to print the Piece's symbol
+  def to_s
+
+  end
+  
+  # * Define symbol method
+  # that represents the piece's symbol
+  def symbol
+    # Won't be implemented in parent class
   end
 
-  def pos=(val)
-    @pos = val
+  # * Define empty? method
+  # to check if this is a Piece, or a NullPiece
+  def empty?
+
+  end
+
+  # * Define valid_moves method
+  # to filter out moves that will put you in check
+  def valid_moves
+
+  end
+
+
+  # * Define move_into_check?(end_pos)
+  # to check if a given move will put you in check
+  def move_into_check?(end_pos)
+
   end
 end
