72b87163ec42bbdc0213099157ef62851cbdbfac
Ryan Tran <ryandtran@hotmail.com>
2020-05-13 18:05:51 -0700

    Added W6D3 classwork

---
+++ W6D3/art_share_api/app/models/artwork.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id        :bigint           not null, primary key
+#  title     :string           not null
+#  image_url :string           not null
+#  artist_id :integer          not null
+#
+class Artwork < ApplicationRecord
+  belongs_to :artist, 
+    foreign_key: :artist_id,
+    class_name: :User
+  
+  has_many :shares,
+    foreign_key: :artwork_id,
+    class_name: :ArtworkShare
+
+  has_many :shared_viewers,
+    through: :shares,
+    source: :viewer
+end
---
+++ W6D3/art_share_api/db/migrate/20200513232924_change_artworks.rb
@@ -0,0 +1,5 @@
+class ChangeArtworks < ActiveRecord::Migration[5.2]
+  def change
+    add_index :artworks, [:title, :artist_id], unique: true
+  end
+end
