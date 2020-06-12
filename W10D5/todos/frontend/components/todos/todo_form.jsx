e73acbc083122165cdc3c2379e75aa11d316f323
Ryan Tran <ryandtran@hotmail.com>
2020-06-11 17:08:41 -0700

    Copied W10D4 project into W10D5

---
+++ W10D5/todos/frontend/components/todos/todo_form.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import uniqueId from '../../util/unique_id'
+
+class TodoForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        id: uniqueId(),
+        title: '',
+        body: '',
+        done: false
+      };
+      this.handleInput = this.handleInput.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(e) {
+      switch (e.target.name) {
+        case 'title':
+          this.setState({title: e.target.value})
+          break;
+        case 'body':
+          this.setState({ body: e.target.value })
+          break;
+        case 'done':
+          this.setState({ done: e.target.checked } )
+          break;                 
+      }
+    }
+
+    handleSubmit(e) {
+
+    }
+
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>Title
+            <input
+              type="text"
+              name="title"
+              onChange={this.handleInput}
+              value={this.state.title}
+            />
+          </label>
+          <br/>
+
+          <label>Body
+            <input 
+              type="text" 
+              name="body"
+              onChange={this.handleInput}
+              value={this.state.body}
+            />
+          </label>
+          <br/>
+          
+          <label>Done
+            <input 
+              type="checkbox"
+              name="done" 
+              onChange={this.handleInput}
+              checked={this.state.done}
+            />
+          </label>
+          <br/>
+
+          <button>Add Item</button>
+        </form>
+      )
+    }
+}
+
+export default TodoForm;
---
+++ W10D5/todos/frontend/todo_redux.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import { receiveTodos, receiveTodo } from './actions/todo_actions';
+import Root from './components/root';
+import allTodos from './reducers/selectors'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('content');
+  const store = configureStore();
+  window.store = store;
+  window.receiveTodo = receiveTodo;
+  window.receiveTodos = receiveTodos;
+  window.allTodos = allTodos
+
+  ReactDOM.render(<Root store={store}/>, root);
+});
