d63c8587dba40387b979955a4776735e0961ae24
Ryan Tran <ryandtran@hotmail.com>
2020-06-17 12:41:01 -0700

    Added front_end_auth project to W11D3

---
+++ W11D3/front_end_auth/frontend/components/session/login.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+ class Signup extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       username: '',
+       password: ''
+     };
+     this.handleSubmit = this.handleSubmit.bind(this);
+   }
+   
+   handleInput(type) {
+     return (e) => {
+       this.setState({ [type]: e.target.value });
+     };
+   }
+
+   handleSubmit(e) {
+     e.preventDefault();
+     this.props.login(this.state)
+      .then( () => this.props.history.push('/chirps'));
+   }
+   
+   render() {
+    return (
+      <div className="session-form">
+        <h2>Login</h2>
+
+        <form>
+          <label>Username:
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.handleInput('username')}
+            />
+          </label>
+          <br/>
+
+          <label>Password:
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.handleInput('password')}
+            />
+          </label>
+          <br/>
+
+          <button onClick={this.handleSubmit}>Sign Up</button>
+        </form>
+      </div>
+    );
+   }
+ }
+
+ export default Signup;
